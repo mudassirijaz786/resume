3e22583359c17a7211f3eeaef3979379157f4a15
Mudassir Ijaz <mudassir.ijaz@kwanso.com>
2021-02-01 18:30:53 +0500

    adding git

---
+++ config.js
@@ -0,0 +1,41 @@
+module.exports = {
+  siteTitle: 'Mudassir Ijaz Portfolio', // <title>
+  manifestName: 'Resume',
+  manifestShortName: 'Landing', // max 12 characters
+  manifestStartUrl: '/',
+  manifestBackgroundColor: '#663399',
+  manifestThemeColor: '#663399',
+  manifestDisplay: 'standalone',
+  manifestIcon: 'src/assets/img/website-icon.png',
+  pathPrefix: `/gatsby-starter-resume/`, // This path is subpath of your hosting https://domain/portfolio
+  firstName: 'Mudassir',
+  lastName: 'Ijaz',
+  joiningCompany: 'Google',
+  currentCompany: 'kwanso',
+  // social
+  socialLinks: [
+    {
+      icon: 'fa-github',
+      name: 'Github',
+      url: 'https://github.com/mudassirijaz786',
+    },
+    {
+      icon: 'fa-linkedin-in',
+      name: 'Linkedin',
+      url: 'https://www.linkedin.com/in/mudassirijaz786/',
+    },
+    {
+      icon: 'fa-twitter',
+      name: 'Twitter',
+      url: 'https://twitter.com/mudassirijaz786',
+    },
+    {
+      icon: 'fa-facebook-f',
+      name: 'Facebook',
+      url: 'https://www.facebook.com/mudassirijaz786',
+    },
+  ],
+  email: 'ijazmudassir786@gmail.com',
+  phone: '+923314068055',
+  address: 'Shahdara, Lahore, Pakistan',
+};
